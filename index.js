62b01e6858922ac8c681067c9db16239974566f2
Taby <tabythiongo@gmail.com>
2026-07-04 23:38:22 +0300

    Complete combineUsers function and pass all tests

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+function combineUsers(arr1 = [], arr2 = [], arr3 = [], arr4 = []) {
+  // 1. merge all arrays
+  const allUsers = [...arr1, ...arr2, ...arr3, ...arr4];
+  
+  // 2. get today's date and format it as M/d/yyyy
+  const today = new Date();
+  const merge_date = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+  
+  // 3. return an object
+  return {
+    users: allUsers,
+    merge_date: merge_date
+  };
+}
 
-
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+// Simple export that works in all labs
+module.exports = { combineUsers };
